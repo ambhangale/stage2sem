754b5bb647ac2a23940ed2763ef4d10c1e4df5bc
ambhangale <148333008+ambhangale@users.noreply.github.com>
2024-05-09 12:43:40 +0200

    preliminary structure

---
+++ functions_stage2sem.R
@@ -0,0 +1,65 @@
+## Aditi M. Bhangale
+## Last updated: 9 May 2024
+
+# " Comparing maximum likelihood to two-stage estimation for structural equation 
+# models of social-network data"
+## Using Nestler et al., 2020 population values
+
+# rm(list = ls())
+
+# setwd("/Users/Aditi_2/Desktop/UvA/SR-SEM_job/stage2sem")
+# getwd()
+
+#################################
+# FUNCTIONS FOR SIMULATIONS ----
+#################################
+
+# function 0: generate level-specific (co)variance matrices----
+
+#----
+
+# function 1: simulate data for a single group----
+
+#----
+
+# function 2: simulate data for multiple groups----
+
+# ----
+
+# function 3: minimum loss function to optimise over to choose hyperparameters for beta priors----
+
+#----
+
+# function 4: thoughtful priors----
+
+#----
+
+# function 5: prophetic priors----
+
+#----
+
+# function 6: ANOVA-based methods-of-moments priors----
+
+#----
+
+# function 7: FIML-based priors----
+
+#----
+
+# function 8: set customised priors for MCMC stage----
+
+#----
+
+# function 9: stage1 in `lavaan.srm`----
+
+#----
+
+# function 10: stage2 SR_SEM in lavaan.srm----
+
+#----
+
+# function 11: FIML for SR-SEM effects in`srm`----
+
+#----
+
+
