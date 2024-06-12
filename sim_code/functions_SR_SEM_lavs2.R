9e78a101ebe81523c083877df20b5a595dd08915
ambhangale <148333008+ambhangale@users.noreply.github.com>
2024-06-12 17:41:04 +0200

    statistics for exact fit

--- sim_code/functions_SR_SEM_lavs2.R
+++ sim_code/functions_SR_SEM_lavs2.R
@@ -1,5 +1,5 @@
 ## Aditi M. Bhangale
-## Last updated: 6 June 2024
+## Last updated: 12 June 2024
 
 # "Comparing maximum likelihood to two-stage estimation for structural equation 
 # models of social-network data"
@@ -25,11 +25,6 @@
 #----
 
 # function 11: stage2 SR_SEM in lavaan.srm----
-
-#TODO figure out how to cbind() MCSampID, n, G, analType, etc to the result
-
-### THE MODEL IS CORRECT, THIS IS JUST HOW NESTLER ET AL., FIT IT
-
 lavs2 <- function(s1ests) {
   library(lavaan.srm)
   
@@ -63,8 +58,8 @@ lavs2 <- function(s1ests) {
   V3_ji ~~ Ivar3*V3_ji
   '
   
-  fit <- lavaan.srm(model = mod, data = s1ests, component = c("case", "dyad"), posterior.est = "mean")
-  # lavaan.srm(model = mod, data = s1ests, component = c("case", "dyad"), posterior.est = "mean", test = c("satorra.bentler","scaled.shifted","browne.residual.adf"))
+  fit <- lavaan.srm(model = mod, data = s1ests, component = c("case", "dyad"), posterior.est = "mean", 
+  test = c("satorra.bentler","scaled.shifted","browne.residual.adf"))
   
   if(lavInspect(fit, "converged")) {
     s2ests <- parameterEstimates(fit)
@@ -80,6 +75,7 @@ lavs2 <- function(s1ests) {
     popVals <- getSigma(return_mats = F)
     s2ests <- merge(s2ests, popVals, by = "par_names")
     s2ests$group <- ifelse(s2ests$group == 1, "case", "dyad")
+    names(s2ests)[names(s2ests) == "group"] <- "level"
     s2ests$MCSampID <- attr(s1ests, "MCSampID")
     s2ests$n <- attr(s1ests, "n"); s2ests$G <- attr(s1ests, "G")
     s2ests$condition <- paste0(attr(s1ests, "n"), "-", attr(s1ests, "G"))
@@ -90,20 +86,99 @@ lavs2 <- function(s1ests) {
     s2ests$coverage <- s2ests$ci.lower < s2ests$pop & s2ests$pop < s2ests$ci.upper
     
     s2ests <- s2ests[, c("MCSampID", "n", "G", "condition", "analType", "s1priorType", 
-                         "s1iter", "s1mPSRF", "par_names", "group", 
+                         "s1iter", "s1mPSRF", "par_names", "level", 
                          "pop", "est", "se", "ci.lower", "ci.upper", "coverage")] # remove non-redundant rows and reorder
     
-    s2mod <- c(MCSampID = attr(s1ests, "MCSampID"), n = attr(s1ests, "n"),
-               G = attr(s1ests, "G"), 
-               condition = paste0(attr(s1ests, "n"), "-", attr(s1ests, "G")), 
-               analType = "2SMLE", 
-               s1priorType  = paste0("MCMC-", attr(s1ests, "priorType"), "-", attr(s1ests, "precision")), 
-               s1iter = attr(s1ests, "iter"), s1mPSRF = attr(s1ests, "mPSRF"),
-               combi.ADF = fit@test$browne.residual.adf$stat, 
-               combi.df = fit@test$browne.residual.adf$df, 
-               combi.p = fit@test$browne.residual.adf$pvalue,
-               case.ADF = fit@test$browne.residual.adf$stat.group[1],
-               dyad.ADF = fit@test$browne.residual.adf$stat.group[2]) # TODO also add columns in s2ests?
+    fitStats <- fit@test # save all test statistics in a single object
+    
+    standard <- c(fitStat.type = fitStats$standard$test, 
+                  combi.stat = fitStats$standard$stat, 
+                  combi.df = fitStats$standard$df, 
+                  combi.p = fitStats$standard$pvalue,
+                  case.stat = fitStats$standard$stat.group[1], 
+                  case.df = 6, case.p = pchisq(fitStats$standard$stat.group[1], 
+                                               df = 6, lower = F),
+                  dyad.stat = fitStats$standard$stat.group[2], 
+                  dyad.df = 12, dyad.p = pchisq(fitStats$standard$stat.group[2], 
+                                                df = 12, lower = F)) # Standard chi-square test
+    
+    adf <- c(fitStat.type = fitStats$browne.residual.adf$test, 
+             combi.stat = fitStats$browne.residual.adf$stat, 
+             combi.df = fitStats$browne.residual.adf$df, 
+             combi.p = fitStats$browne.residual.adf$pvalue,
+             case.stat = fitStats$browne.residual.adf$stat.group[1], 
+             case.df = 6, case.p = pchisq(fitStats$browne.residual.adf$stat.group[1], 
+                                          df = 6, lower = F),
+             dyad.stat = fitStats$browne.residual.adf$stat.group[2], 
+             dyad.df = 12, dyad.p = pchisq(fitStats$browne.residual.adf$stat.group[2], 
+                                           df = 12, lower = F)) # Browne's residual-based ADF
+    
+    sb <- c(fitStat.type = fitStats$satorra.bentler$test, 
+            combi.stat = fitStats$satorra.bentler$stat, 
+            combi.df = fitStats$satorra.bentler$df, 
+            combi.p = fitStats$satorra.bentler$pvalue,
+            case.stat = fitStats$satorra.bentler$stat.group[1], 
+            case.df = 6, case.p = pchisq(fitStats$satorra.bentler$stat.group[1], 
+                                         df = 6, lower = F),
+            dyad.stat = fitStats$satorra.bentler$stat.group[2], 
+            dyad.df = 12, dyad.p = pchisq(fitStats$satorra.bentler$stat.group[2], 
+                                          df = 12, lower = F)) # Satorra and Bentler's corrected statistic
+    ss <- c(fitStat.type = fitStats$scaled.shifted$test, 
+            combi.stat = fitStats$scaled.shifted$stat, 
+            combi.df = fitStats$scaled.shifted$df, 
+            combi.p = fitStats$scaled.shifted$pvalue,
+            case.stat = fitStats$scaled.shifted$stat.group[1], 
+            case.df = 6, case.p = pchisq(fitStats$scaled.shifted$stat.group[1], 
+                                         df = 6, lower = F),
+            dyad.stat = fitStats$scaled.shifted$stat.group[2], 
+            dyad.df = 12, dyad.p = pchisq(fitStats$scaled.shifted$stat.group[2], 
+                                          df = 12, lower = F)) # Scaled-shifted statistic
+    
+    Ncase <- attr(s1ests, "nobs")["case"]; Ndyad <- attr(s1ests, "nobs")["dyad"]
+    Ntotal <- Ncase + Ndyad
+    
+    yb_corrected.combistat <- fitStats$browne.residual.adf$stat / (1 + (fitStats$browne.residual.adf$stat/Ntotal))
+    yb_corrected.casestat <- fitStats$browne.residual.adf$stat.group[1] / (1 + (fitStats$browne.residual.adf$stat.group[1]/(Ncase)))
+    yb_corrected.dyadstat <- fitStats$browne.residual.adf$stat.group[2] / (1 + (fitStats$browne.residual.adf$stat.group[2]/(Ndyad)))
+    yb_corrected.combip <- pchisq(yb_corrected.combistat, df = 18, lower = F)
+    yb_corrected.casep <- pchisq(yb_corrected.casestat, df = 6, lower = F)
+    yb_corrected.dyadp <- pchisq(yb_corrected.dyadstat, df = 12, lower = F)
+    yb_corrected <- c(fitStat.type = "yuan.bentler.corrected.adf", 
+                  combi.stat = yb_corrected.combistat, 
+                  combi.df = 18, 
+                  combi.p = yb_corrected.combip,
+                  case.stat = yb_corrected.casestat, 
+                  case.df = 6, case.p = yb_corrected.casep,
+                  dyad.stat = yb_corrected.dyadstat, 
+                  dyad.df = 12, dyad.p = yb_corrected.dyadp) # Yuan and Bentler's small-sample correction for ADF
+    
+    
+    yb_F.combistat <- ((Ntotal - 18)/(Ntotal*18))*fitStats$browne.residual.adf$stat
+    yb_F.casestat <- ((Ncase - 6)/(Ntotal*6))*fitStats$browne.residual.adf$stat.group[1]
+    yb_F.dyadstat <- ((Ncase - 12)/(Ntotal*12))*fitStats$browne.residual.adf$stat.group[2]
+    yb_F.combip <- pchisq(yb_F.combistat, df = 18, lower = F)
+    yb_F.casep <- pchisq(yb_F.casestat, df = 6, lower = F)
+    yb_F.dyadp <- pchisq(yb_F.dyadstat, df = 12, lower = F)
+    yb_F <- c(fitStat.type = "yuan.bentler.F", 
+              combi.stat = yb_F.combistat, 
+              combi.df = paste0(18, ",", (Ntotal-18)), 
+              combi.p = yb_F.combip,
+              case.stat = yb_F.casestat, 
+              case.df = paste0(6, ",", (Ncase-6)), case.p = yb_F.casep,
+              dyad.stat = yb_F.dyadstat, 
+              dyad.df = paste0(12, ",", (Ndyad-12)), dyad.p = yb_F.dyadp) # Yuan and Bentler's F statistic based on ADF
+    
+    s2mod <- data.frame(cbind(MCSampID = attr(s1ests, "MCSampID"),
+                              n = attr(s1ests, "n"),
+                              G = attr(s1ests, "G"),
+                              condition = paste0(attr(s1ests, "n"), "-", attr(s1ests, "G")),
+                              analType = "2SMLE",
+                              s1priorType = paste0("MCMC-", attr(s1ests, "priorType"), "-", attr(s1ests, "precision")),
+                              s1iter = attr(s1ests, "iter"),
+                              s1mPSRF = attr(s1ests, "mPSRF"), rbind(standard, adf, sb, ss, yb_corrected, yb_F)))
+    
+    
+    #TODO don't forget to add redundant columns to make sure you can rbind() with the ogsrm() output later on
     
   } else {
     s2result <- NULL
